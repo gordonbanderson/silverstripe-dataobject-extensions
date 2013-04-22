1013f957c03775c28067460843c25ecd49540152
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-04-22 17:50:11 +0700

    UPGRADE: Changed decorator to extension in reference filename

--- _config.php
+++ _config.php
@@ -3,6 +3,6 @@
 // It would be nice to add this to ViewableData rather than DataObject,
 // but methods added to ViewableData using add_extension are not available
 // on DataObjects
-Object::add_extension('DataObject','ExtsDataObjectDecorator');
+Object::add_extension('DataObject','ExtsDataObjectExtension');
 
-Object::add_extension('SiteTree','ExtsSiteTreeDecorator');
+Object::add_extension('SiteTree','ExtsSiteTreeExtension');
